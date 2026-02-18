d1398e9d5cbef5f1be820d56d30b2f3cf43e3d26
josedussan <josedussan1996@hotmail.com>
2026-02-18 10:23:11 -0500

    se crea UI, script y animacion de transicion entre escenas

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] Animator transicion;
+    [SerializeField] float timeTransition = 1f;
+    public void LoadScene(string scene)
+    {
+        StartCoroutine(LoadLevel(scene));
+    }
+
+    IEnumerator LoadLevel(string scene)
+    {
+        transicion.SetTrigger("Start");
+
+        yield return new WaitForSeconds(timeTransition);
+        SceneManager.LoadScene(scene);
+    }
+
+}
